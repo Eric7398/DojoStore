d1642eff9a1faa8f5bb11eb4c1513ac102fa824b
Eric7398 <ericwang7398@gmail.com>
2021-01-27 17:26:58 -0800

    Added Login/Logout

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema({
     name: {
@@ -24,6 +25,24 @@ const userSchema = mongoose.Schema({
 }, {
     timestamps: true,
 })
+
+// Bcrypt to match the password with database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+userSchema.pre('save', async function (next) {
+    // When on editing page you still want them to be logged in
+    if (!this.isModified('password')) {
+        next()
+    }
+    // We are salting and hashing the password for bcrypt
+    const salt = await bcrypt.genSalt(10)
+    // Setting plain text password to the hashed password.
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+
 
 const User = mongoose.model('User', userSchema)
 
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -10,11 +11,15 @@ connectDB()
 
 const app = express();
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API is Running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+
 
 
 // For all Errors that are 404 not found errors
